f38bf0515c4d4e04d3c189792e5c10034f25022c
galen robbins <galenrobbins@Mac.lan>
2025-03-28 12:14:21 -0500

    implement RecurringOrder get/post

--- src/api.py
+++ src/api.py
@@ -1,33 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, encoders, HTTPException
+from .model import RecurringOrder, User
+from typing import Literal
+from decimal import Decimal
+from pydantic import BaseModel, ValidationError
 
+import traceback
 
 app = FastAPI(
     title="Investifi Backend Coding Challenge",
 )
 
-
-@app.get("/")
-def hello_world():
-    """
-    NOTE: This is route is used as an example for the test suite
-    No action needed here
-    """
-    return {"hello": "world"}
-
+class RecurringOrderPublic(BaseModel):
+    asset_type: str
+    frequency: str
+    amount: Decimal
+    user_id: str
 
 @app.get("/recurring-orders")
-def get_recurring_orders():
+def get_recurring_orders(user_id: str = Query(...)):
     """
     TODO
     # Requirements:
     # The GET route should accept a User ID and return only said users recurring orders
     # if no ID is provided, an error should be raised.
     """
-    return {}
+    if not user_id:
+        raise HTTPException(status_code=400, detail="user_id is required")
+
+    try:
+        db_orders = RecurringOrder.query(user_id)
+        orders = [RecurringOrderPublic(
+            asset_type=order.range_key.split(":")[1],
+            frequency=order.range_key.split(":")[0],
+            amount=order.currency_amount,
+            user_id=order.hash_key,
+        ) for order in db_orders]
+        return {"message": ([encoders.jsonable_encoder(o) for o in orders])}
+    except Exception as ex:
+        raise HTTPException(status_code=400, detail=str(ex))
 
 
 @app.post("/recurring-orders")
-def post_recurring_orders():
+def post_recurring_orders(order: RecurringOrderPublic):
     """
     TODO
     Requirements:
@@ -38,4 +52,19 @@ def post_recurring_orders():
     A recurring order must have a USD amount greater than 0
     A recurring order needs to be associated with a specifc user.
     """
-    return {}
+    db_users = User.query(order.user_id)
+    users = [user for user in db_users]
+    if len(users) < 1:
+        raise HTTPException(status_code=400, detail="User does not exist")
+
+    try:
+        range_key = f'{order.frequency}:{order.asset_type}'
+        RecurringOrder(
+            hash_key=order.user_id,
+            range_key=range_key,
+            currency="USD",
+            currency_amount=order.amount,
+        ).save()
+        return {"message": "Recurring order created successfully"}
+    except Exception as ex:
+        raise HTTPException(status_code=400, detail=str(ex))
--- src/model.py
+++ src/model.py
@@ -1,8 +1,11 @@
 import os
 from abc import abstractmethod
 from dyntastic import Dyntastic
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 from typing import Optional
+from decimal import Decimal
+from enum import Enum
+import boto3
 
 
 class DynamoDbModelBase(Dyntastic):
@@ -34,11 +37,62 @@ class UserInfo(BaseModel):
     first_name: str
     last_name: str
 
-
 class User(DynamoDbModelBase):
     __table_name__ = "User"
+    hash_key = str
     info: Optional[UserInfo]
 
 
+# Relevant
+
+class AssetType(str, Enum):
+    BTC = "BTC"
+    ETH = "ETH"
+
+class Frequency(str, Enum):
+    DAILY = "DAILY"
+    BIMONTHLY = "BIMONTHLY"
+
+class Currency(str, Enum):
+    USD = "USD"
+    
 class RecurringOrder(DynamoDbModelBase):
     __table_name__ = "RecurringOrder"
+    __dynamodb__ = boto3.resource("dynamodb", 
+        region_name=os.environ.get("AWS_REGION"),
+        endpoint_url=os.environ.get("DYNAMO_ENDPOINT"),  
+        aws_access_key_id="dummy",
+        aws_secret_access_key="dummy",
+    )
+    __boto_table__ = __dynamodb__.Table(__table_name__)
+
+    hash_key: str # user_id
+    range_key: str # Frequency:AssetType
+    currency: Currency
+    currency_amount: Decimal
+
+    @validator("currency_amount")
+    def amount_must_be_positive(cls, v):
+        if v <= 0:
+            raise ValueError("Currency amount must be greater than 0")
+        return v
+
+    @validator("range_key")
+    def validate_range_key(cls, v):
+        freq, asset = v.split(":")
+        if freq not in Frequency.__members__:
+            raise ValueError(f"Frequency must be one of {','.join(e.value for e in Frequency)}")
+        if asset not in AssetType.__members__:
+            raise ValueError(f"Asset must be one of {','.join(e.value for e in AssetType)}")
+        return v
+
+    def save(self): # Overriding the default save
+        item = self.dict()
+        try:
+            self.__boto_table__.put_item(
+                Item=item,
+                ConditionExpression="attribute_not_exists(hash_key) AND attribute_not_exists(range_key)",
+            )
+        except Exception as e:
+            # This could be made better
+            raise ValueError("Recurring order already exists") from e
